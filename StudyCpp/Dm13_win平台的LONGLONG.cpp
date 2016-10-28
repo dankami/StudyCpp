3262381f626727e7cf58d3017a9d70e808f70b40
dankami <763757511@qq.com>
2016-10-28 09:28:25 +0800

    更新DM

---
+++ StudyCpp/Dm13_win平台的LONGLONG.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>   // win平台的东西，唉
+#include "minwindef.h"
+#include "winnt.h"
+
+using namespace std;
+
+int main()
+{
+	LONGLONG ll = 10;   // 要引入win平台的东西
+	WORD w = '1';
+	BYTE b = 25;
+	bool bl = true;
+	bool b4[4];
+	WORD w4[4];
+	cout << "sizeof(LONGLONG) = " << sizeof(ll) << endl;	// 长度为8
+	cout << "sizeof(WORD) = " << sizeof(w) << endl;			// 长度为2
+	cout << "sizeof(BYTE) = " << sizeof(b) << endl;			// 长度为1
+	cout << "sizeof(bool) = " << sizeof(bl) << endl;		// 长度为1
+	cout << "sizeof(bool[4]) = " << sizeof(b4) << endl;		// 长度为4
+	cout << "sizeof(WORD[4]) = " << sizeof(w4) << endl;		// 长度为8
+	system("pause");
+	return 0;
+}
